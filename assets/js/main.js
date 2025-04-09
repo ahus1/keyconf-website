f5d79d0e932c6a376f63bb2e969800e80cc795b9
Alexander Schwartz <alexander.schwartz@gmx.net>
2025-04-09 11:56:28 +0200

    Registration is now open

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,7 @@ var spy = new Gumshoe('#navigation a', {
 
 /* ======= Countdown ========= */
 // set the date we're counting down to
-var target_date = new Date("2024-09-19T10:00").getTime();
+var target_date = new Date("2025-08-28T10:00").getTime();
  
 // variables for time units
 var days, hours, minutes, seconds;
